d43629b93fc92952e36528a60e228018615a91e8
David Rosero C <davidroserocalle@gmail.com>
2020-03-03 14:45:46 -0500

    Update file, create the basic structure of the project, optimiced code

--- dfc_pkg/deploy/app_server.py
+++ dfc_pkg/deploy/app_server.py
@@ -1,50 +1,64 @@
 #!/usr/bin/python3
+from datetime import datetime
+from invoke import run as local
 import os
 import typer
-app = typer.Typer()
 
+
+
+app = typer.Typer()
+time = "%Y-%m-%dT%H-%M"
 path_proyect = os.getenv("DFC_PATH")
 
+
+def verify_path(path: str):
+    """
+    This function verify the project folder
+    :param path: path to verify the project
+    :return: void
+    """
+    path_web = path + "/web"
+    path_api = path + "/api"
+    path_test = path + "/tests"
+    path_models = path + "/models"
+    requirements = path + "/requirements.txt"
+
+    if os.path.isdir(path_web) is False:
+        typer.echo(f"You need to create the directory web in: {path}")
+        exit(0)
+    if os.path.isdir(path_api) is False:
+        typer.echo(f"You need to create the directory api in: {path}")
+        exit(0)
+    if os.path.isdir(path_models) is False:
+        typer.echo(f"You need to create the directory models in: {path}")
+        exit(0)
+    if os.path.isdir(path_test) is False:
+        typer.echo(f"You need to create the directory test in: {path}")
+        exit(0)
+    if os.path.isfile(requirements) is False:
+        typer.echo("You need the requirements file to install all the dependencies in the web server")
+        exit(0)
+
+
+def compress(path: str):
+    my_date = datetime.now().strftime(time)
+    split_path = path.split("/")
+    version_path = path + "/versions"
+    name = split_path[len(split_path) - 1]
+    zip_file = my_date + "-" + name + ".tgz"
+    local(f'mkdir -p {version_path}')
+    local(f'tar -zcvf {version_path}/{zip_file} {path}')
+
+
 @app.command()
-def app_server(ip: str = typer.Option(...)):
+def app_server(ip: str, path_key: str, user_ssh: str):
     if path_proyect is None:
-        typer.echo("Checking the current path project")
         os.chdir(os.path.dirname(__file__))
-        path_web = os.getcwd() + "/web"
-        path_api = os.getcwd() + "/api"
-        path_models = os.getcwd() + "/models"
-        requirements = os.getcwd() + "/requirements.txt"
-
-        if os.path.isdir(path_web) is False:
-            typer.echo(f"You need to create the directory web in: {os.getcwd()}")
-            exit(0)
-        if os.path.isdir(path_api) is False:
-            typer.echo(f"You need to create the directory api in: {os.getcwd()}")
-            exit(0)
-        if os.path.isdir(path_models) is False:
-            typer.echo(f"you need to create the directory models in: {os.getcwd()}")
-            exit(0)
-        if os.path.isfile(requirements) is False:
-            typer.echo("You need the requirements file to install all the dependencies in the web server")
-            exit(0)
+        verify_path(os.getcwd())
     else:
-        path_web = path_proyect + "/web"
-        path_api = path_proyect + "/api"
-        path_models = path_proyect + "/models"
-        requirements = path_proyect + "/requirements.txt"
+        verify_path(path_proyect)
+        compress(path_proyect)
 
-        if os.path.isdir(path_web) is False:
-            typer.echo(f"You need to create the directory web in: {path_proyect}")
-            exit(0)
-        if os.path.isdir(path_api) is False:
-            typer.echo(f"You need to create the directory api in: {path_proyect}")
-            exit(0)
-        if os.path.isdir(path_models) is False:
-            typer.echo(f"you need to create the directory models in: {os.getcwd()}")
-            exit(0)
-        if os.path.isfile(requirements) is False:
-            typer.echo("You need the requirements file to install all the dependencies in the web server")
-            exit(0)
 
 
 if __name__ == "__main__":
