47530a5e55a63f94e8a8573cc63c00db02c617b0
David Rosero C <davidroserocalle@gmail.com>
2020-03-04 23:19:07 +0000

    Rename the files app_server for nginx_project: for a good managment create the directory dpl this folder get any functions for this project

--- dfc_pkg/main.py
+++ dfc_pkg/main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import typer
-import deploy.app_server as app_server
+import deploy.nginx_project as nginx_project
 
 app = typer.Typer()
 
-app.add_typer(app_server.app, name="deploy")
+app.add_typer(nginx_project.app, name="deploy")
 
 if __name__ == "__main__":
     app()
