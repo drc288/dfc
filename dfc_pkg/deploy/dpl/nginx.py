233f4e526ab7b686c339d07d7f97c02faf706107
David Rosero C <davidroserocalle@gmail.com>
2020-03-05 13:08:01 -0500

    Add the functionality gunicorn for the project, next step, create gunicorn as a service, becouse the proces stop in this part

--- dfc_pkg/deploy/dpl/nginx.py
+++ dfc_pkg/deploy/dpl/nginx.py
@@ -14,11 +14,12 @@ def install_nginx(server, user: str, ip: str):
     """
     try:
         # To remove the error =  debconf: unable to initialize frontend: Dialog
-        # server.run("sudo sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile")
+        server.run("sudo sed -i 's/^mesg n$/tty -s \&\& mesg n/g' /root/.profile")
         server.run("sudo apt-get update -y > /dev/null")
         typer.echo(stylize("Server updated", fg("green"), attr("bold")))
         server.run("sudo apt-get install nginx -y > /dev/null")
         server.run("sudo apt-get install python3-pip -y > /dev/null")
+        server.run("sudo pip3 install -U pip")
         typer.echo(stylize("NGINX and PIP are installed ", fg("green"), attr("bold")))
     except socket.error:
         typer.echo(stylize(f'Unable to connect to {user}@{ip}', fg("red")))
@@ -52,18 +53,27 @@ def config_nginx(server, path: str, zip_file: str, user: str):
         path_split = path.split("/")
         remove_path = path_split[1]
         path_project = path_split[len(path_split) - 1]
+        # Create the folder /data/ descompress the zip file in /data/
         server.run(f"sudo mkdir -p /data/")
         server.run(f"sudo tar -xzf /tmp/{zip_file} -C /data/")
-        # Move project folder and remove trash files
+        # Move the path project to /data/
         server.run(f"sudo mv /data{path} /data/")
-        server.run(f"sudo rm -rf /data/{remove_path}/")
-        server.run(f"sudo rm -rf /tmp/{zip_file}")
-        # add permissions
+        # Remove trash files in /data/DESS_FILE and the tmp
+        server.run(f"sudo rm -rf /data/{remove_path}/ /tmp/{zip_file}")
+        # Change the user and group owner of /data/
         server.run(f"sudo chown -R {user}. /data")
-        server.run(f"sudo chown -R {user} /home/{user}/.cache/pip/")
-        # Install dependencies in pip3
-        server.run(f"sudo pip3 install /data/{path_project}/requirements.txt")
+        server.run(f"sudo pip install  -r /data/{path_project}/requirements.txt > /dev/null")
         typer.echo(stylize("Modules are installed", fg("green"), attr("bold")))
         typer.echo(stylize("NGINX configured", fg("green"), attr("bold")))
     except socket.error:
         typer.echo(stylize("An error has occurred in the configuration NGINX", fg("red")))
+
+
+def run_gunicorn(server, path: str, port: int):
+    path_split = path.split("/")
+    name_path = path_split[len(path_split) - 1]
+    try:
+        server.run(f"cd /data/{name_path} && gunicorn -b 0.0.0.0:{port} web.app:app &> /dev/null &", hide=True)
+        typer.echo(stylize(f"Server are excecuted in the port: {port}", fg("green"), attr("bold")))
+    except socket.error:
+        typer.echo(stylize("An error has occurred in the configuration GUNICORN", fg("red")))
--- dfc_pkg/deploy/nginx_project.py
+++ dfc_pkg/deploy/nginx_project.py
@@ -2,7 +2,7 @@
 from deploy.dpl.verify_path import verify_path
 from deploy.dpl.compress import compress
 from deploy.dpl.create_connection import create_connection
-from deploy.dpl.nginx import install_nginx, upload_files, config_nginx
+from deploy.dpl.nginx import install_nginx, upload_files, config_nginx, run_gunicorn
 import os
 import typer
 
@@ -12,12 +12,13 @@ path_proyect = os.getenv("DFC_PATH")
 
 @app.command()
 def nginx_project(ip: str = typer.Option(...), path_key: str = typer.Option(...),
-               user_ssh: str = typer.Option(...)):
+                  user_ssh: str = typer.Option(...), port: int = typer.Option(...)):
     """
     create a app server, connect to host an deploy the services in nginx server
     :param ip: host to connect
     :param path_key: ssh key
     :param user_ssh: user to connect
+    :param port: the number of the port to run gunicorn
     :return: deploy server
     """
     if path_proyect is None:
@@ -34,6 +35,8 @@ def nginx_project(ip: str = typer.Option(...), path_key: str = typer.Option(...)
         install_nginx(server, user_ssh, ip)
         upload_files(server, path_proyect, zip_file)
         config_nginx(server, path_proyect, zip_file, user_ssh)
+        run_gunicorn(server, path_proyect, port)
+
 
 
 if __name__ == "__main__":
